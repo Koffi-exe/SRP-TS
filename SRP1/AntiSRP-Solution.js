1d1ad2c738df3c2586446263d894bdc0d17d4d4f
Koffi-exe <adithyaasasi@gmail.com>
2025-07-08 20:30:52 +0530

    first commit

---
+++ SRP1/AntiSRP-Solution.js
@@ -0,0 +1,48 @@
+"use strict";
+// interface Iuser {
+//   name: string;
+//   email: string;
+// }
+// class UserManager {
+//   constructor(private db: Database) {}
+//   createUser(user: Iuser) {
+//     try {
+//       new ValidataUser(user).validate();
+//       this.db.users.push(user);
+//       new SendEmail(user).sendEmail();
+//       console.log("user created successfully!");
+//     } catch (error) {
+//       console.log("Failed to create user", error);
+//     }
+//   }
+// }
+// class Database {
+//   users: Iuser[] = [];
+// }
+// class ValidataUser {
+//   constructor(private user: Iuser) {}
+//   validate() {
+//     if (!this.user.name || this.user.email.indexOf("@") === -1) {
+//       throw new Error("Invalid user");
+//     }
+//     console.log("User validated")
+//   }
+// }
+// class SendEmail {
+//   constructor(private user: Iuser) {}
+//   sendEmail() {
+//     console.log("Sending email to, ", this.user.email);
+//   }
+// }
+// class FetchUser {
+//   constructor(private db: Database) {}
+//   getAllUsers(): Iuser[] {
+//     return this.db.users;
+//   }
+// }
+// /// Usage;
+// const db = new Database();
+// const manager = new UserManager(db);
+// manager.createUser({ name: "Adithya", email: "adithy@ad.com" });
+// const Fetch = new FetchUser(db);
+// console.log(Fetch.getAllUsers());
---
+++ SRP1/AntiSRP1.js
@@ -0,0 +1,29 @@
+"use strict";
+// // Violates SRP: Too many responsibilities
+// interface User {
+//   name: string;
+//   email: string;
+// }
+// class UserManager {
+//   private users: User[] = [];
+//   createUser(user: User): void {
+//     // 1. Validate user
+//     if (!user.name || !user.email.includes("@")) {
+//       throw new Error("Invalid user");
+//     }
+//     // 2. Save to "DB"
+//     this.users.push(user);
+//     // 3. Send email
+//     this.sendEmail(user.email, "Welcome to the app!");
+//   }
+//   private sendEmail(to: string, message: string): void {
+//     // Simulated email sending
+//     console.log(`Email sent to ${to}: ${message}`);
+//   }
+//   getAllUsers(): User[] {
+//     return this.users;
+//   }
+// }
+// const manager = new UserManager();
+// manager.createUser({ name: "Adithya", email: "adithya@example.com" });
+// console.log(manager.getAllUsers());
